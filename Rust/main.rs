210d1dcfe885e1a8890d979e5a338edf61f65528
Jacob Holst <hol19002@byui.edu>
2023-11-24 23:19:33 -0700

    Solved some type errors, made sure references to functions all work properly; made a continuous loop until user quits

--- Rust/main.rs
+++ Rust/main.rs
@@ -3,23 +3,46 @@
 fn main(){
     let mut name = String::new();
     println!("What's your name?");
-    let b1 = std::io::stdin().read_line(&mut name).unwrap();
+    let _ = std::io::stdin().read_line(&mut name).unwrap();
     println!("Welcome to the password generator, {}", name);
 
-    // Send the user to the main menu
-    menu()
+    // Send the user to the main menu, keeping them there until they select quit
+    while menu() == true{
+        println!("-----------------------------")
+    }
 
 }
 
-fn menu(){
+fn menu() -> bool{
+
     // Print out the options
-    /*1. Make new password
-    2. Edit a password
-    3. Generate a password*/
     let mut ans = String::new();
-    println!("Please select an option below: ");
-    let b1 = std::io::stdin().read_line(&mut ans).unwrap();
-    // match {}
+    println!("Please select an option below: 
+    [1] Make a new password
+    [2] Edit a password
+    [3] Generate a password
+    [4] Quit
+    ");
+
+    // Process the input
+    let _ = std::io::stdin().read_line(&mut ans).unwrap();
+    match ans.trim_end(){
+        "1" => { println!("What will the password be?");
+                    new_password();
+                    return true;
+                },
+        "2" => { println!("Which account would you like to change the password for?");
+                    edit_password();
+                    return true;
+                },
+        "3" => { println!("You're password today is: schlindikrop15");
+                    generate_password();
+                    return true;
+                },
+        _ => {println!("Byyyyyyye!");
+                return false;
+            }
+    }
 }
 
 fn new_password(){
