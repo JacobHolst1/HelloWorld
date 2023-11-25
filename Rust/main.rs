bae5f094667402cb7459d8bf7770ecdd958bae0e
Jacob Holst <hol19002@byui.edu>
2023-11-24 18:10:35 -0700

    Implemented first input statements and outlined some functions

--- Rust/main.rs
+++ Rust/main.rs
@@ -1,3 +1,42 @@
+// use dict::{Dict, DictIface};
+
 fn main(){
-    println!("Hello world!");
+    let mut name = String::new();
+    println!("What's your name?");
+    let b1 = std::io::stdin().read_line(&mut name).unwrap();
+    println!("Welcome to the password generator, {}", name);
+
+    // Send the user to the main menu
+    menu()
+
+}
+
+fn menu(){
+    // Print out the options
+    /*1. Make new password
+    2. Edit a password
+    3. Generate a password*/
+    let mut ans = String::new();
+    println!("Please select an option below: ");
+    let b1 = std::io::stdin().read_line(&mut ans).unwrap();
+    // match {}
+}
+
+fn new_password(){
+    // Get input from the user for the application/site/whatever linked with the password
+    println!("Welcome to password generator!")
+
+    // Input the password. If it isn't very secure, prompt the user if they want to keep it
+
+
+}
+
+fn edit_password(){
+    // Which password do we want to edit?
+
+    // Make a new password to replace it
+}
+
+fn generate_password(){
+
 }
